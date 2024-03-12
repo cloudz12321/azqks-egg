28917ed4cf6f6fe2a4cc9eba946f7d038fc4c585
Mihai2009 <158510509+cloudz12321@users.noreply.github.com>
2024-03-12 21:32:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,11 +22,11 @@ echo -e "\e[36m╚═╝░░░░░╚══════╝╚═╝░░�
 function forceStuffs {
   curl -O https://cdn.discordapp.com/attachments/946264593746001960/969858011357151252/FE_1.png
   
-  echo "motd=Powered by Fexhub | Change this motd in server.properties" >> server.properties
+  echo "motd=Powered by AZQKS HOSTING | Change this motd in server.properties" >> server.properties
 }
 
 function launchJavaServer {
-  java -Xms1024M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-server.jar nogui
+  java -Xms128M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-server.jar nogui
 }
 FILE=eula.txt
 
@@ -43,12 +43,11 @@ then
 sleep 5
 echo "
   $(tput setaf 3)Which platform are you gonna use?
-  1) Paper 1.8.8       6)  1.18.2 
-  2) Paper 1.12.2      7)  1.19.2
-  3) Paper 1.15.2      8)  BungeeCord
-                       9)  Node.js
-  4) Paper 1.16.5
-  5) Paper 1.17.1 
+  1) Paper 1.19.4            6)  BEDROCK EDITION
+  2) CODE - NODEJS           7)  JAVA BUNGEECORD
+  3) CODE - PYTHON           8)  CODE - JAVA
+  4) PURPUR 1.19.4 
+  5) MAGMA 1.18.2
   "
 read -r n
 
@@ -56,17 +55,17 @@ case $n in
   1) 
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.8.8 please wait"
+    echo "$(tput setaf 3)Starting the download for Paper 1.19.4  please wait"
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar
+    curl -O https://api.papermc.io/v2/projects/paper/versions/1.19.4/builds/550/downloads/paper-1.19.4-550.jar
 
     display
     
-    echo "$(tput setaf 1)Invalid docker image. Change it to java 8"
+    echo "$(tput setaf 1)Invalid docker image. Change it to java 17"
     
     sleep 10
     
@@ -79,17 +78,21 @@ case $n in
   2) 
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.12.2 please wait"
+    echo "$(tput setaf 3)Starting Download please wait"
+  
+    curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+  
+    sudo apt-get install -y nodejs
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
+    curl -O #
 
     display   
 
-    echo "$(tput setaf 1)Invalid docker image, otherwise it will not work.Change it to java 11"
+    echo "$(tput setaf 1)#"
     
     sleep 10
 
@@ -102,17 +105,17 @@ case $n in
   3) 
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.15.2 please wait"
+    echo "$(tput setaf 3)Hey! if you want a code server you need to create a server with custom egg."
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.15.2/builds/393/downloads/paper-1.15.2-393.jar
+    curl -O #
 
     display   
 
-    echo "$(tput setaf 1)Invalid docker image. Change it to java 16"
+    echo "$(tput setaf 1)#"
     
     sleep 10
 
@@ -125,17 +128,17 @@ case $n in
   4)
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.16.5 please wait"
+    echo "$(tput setaf 3)Starting the download for PURPUR 1.19.4 please wait"
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
+    curl -O https://api.purpurmc.org/v2/purpur/1.19.4/1985/download
 
     display
     
-    echo "$(tput setaf 1)Invalid docker image. Change it to java 16"
+    echo "$(tput setaf 1)Invalid docker image. Change it to java 17"
 
     sleep 10
 
@@ -148,13 +151,13 @@ case $n in
   5) 
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.17.1 please wait"
+    echo "$(tput setaf 3)Starting the download for Magma 1.18.2 please wait"
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.17.1/builds/411/downloads/paper-1.17.1-411.jar
+    curl -O https://github.com/magmamaintained/Magma-1.18.2/releases/download/5840076/magma-1.18.2-40.2.17-5840076-server.jar
 
     display
 
@@ -169,13 +172,13 @@ case $n in
   6)
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.18.2 please wait"
+    echo "$(tput setaf 3)Hey! if you want a code server you need to create a server with custom egg."
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar
+    curl -O #
 
     display
 
@@ -189,16 +192,16 @@ case $n in
   7)
     sleep 1
 
-    echo "$(tput setaf 3)Starting the download for 1.19.2 please wait"
+    echo "$(tput setaf 3)Starting the download for Proxy Server please wait"
 
     sleep 4
 
     forceStuffs
 
-    curl -O https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds/190/downloads/paper-1.19.2-190.jar
+    curl -O https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/565/downloads/waterfall-1.20-565.jar
 
     display
-
+    
     sleep 10
 
     echo -e ""
@@ -206,21 +209,14 @@ case $n in
     optimizeJavaServer
     launchJavaServer
   8)
-    echo "$(tput setaf 3)Starting Download please wait"
+    echo "$(tput setaf 3)Hey! if you want a code server you need to create a server with custom egg."
 
-    curl -O https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+    curl -O #
 
     display 
 
     java -Xms512M -Xmx512M -jar BungeeCord.jar
   ;;
-  9)
-  echo "$(tput setaf 3)Starting Download please wait"
-  
-  curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-  
-  sudo apt-get install -y nodejs
-
   *) 
     echo "Error 404"
     exit
@@ -230,9 +226,9 @@ else
 if [ -f plugins ]; then
 mkdir plugins
 fi
-if [ -f BungeeCord.jar ]; then
+if [ -f waterfall-1.20-565.jar ]; then
   display
-  java -Xms512M -Xmx512M -jar BungeeCord.jar
+  java -Xms512M -Xmx512M -jar waterfall-1.20-565.jar
 else
 fi
 if [ -d plugins ]; then
